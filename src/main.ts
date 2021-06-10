4a9da92df67499fa440283a66f11daa5e29664f4
trannam96 <32139405+trannam96@users.noreply.github.com>
2021-06-10 23:15:13 +0700

    refactor code

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const mailMircoservice = app.connectMicroservice<MicroserviceOptions>({
+  app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: { retryAttempts: 1, retryDelay: 3000, port: Number(process.env.MAIL_SERVICE_PORT) },
   });
